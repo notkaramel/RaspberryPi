e05ac275320a01ac8059d65141a47b9677365a72
Antoine Phan from RPi400 <antoine.phan@mail.mcgill.ca>
2022-07-31 00:58:02 -0400

    Minor update

--- segmented_7_5161BS_timer.py
+++ segmented_7_5161BS_timer.py
@@ -5,7 +5,7 @@ def main():
     setup()
     try:
         while(True):
-            for i in range (1,10):
+            for i in range(10):
                 displayNumber(i)
                 sleep(1)
     except KeyboardInterrupt:
